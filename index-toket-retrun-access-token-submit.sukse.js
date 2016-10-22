9d99877716404d3a1db7e2deda87088457d19c46
tirtamubien <tirtamubien@gmail.com>
2016-10-22 15:14:03 +0700

    Add files via upload

---
+++ index-toket-retrun-access-token-submit.sukse.js
@@ -0,0 +1,13 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function()
+{
+var aa=$("#mycredit").val();
+if (aa == null) {
+window.location.href = "http://tirtamubien.com/";
+};
+$("#mycredit").attr("href","
+http://tirtamubien.com/");
+});
+//]]>
+</script>
