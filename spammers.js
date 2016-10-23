8477617c1ea28b614eaa636d54984e842ea817f3
tirtamubien <tirtamubien@gmail.com>
2016-10-23 13:21:57 +0700

    Add files via upload

---
+++ spammers.js
@@ -0,0 +1,494 @@
+
+
+<html>
+
+<head>
+    <title>Hii Spammer</title>
+	<link rel="shortcut icon" href="https://4.bp.blogspot.com/-WjJkj8-L3lo/WAt7Lt1XsGI/AAAAAAAACnI/jlL0ncl809cOMN-ltaZiIsbbKV5hJplVACLcB/s1600/tirtamubien.png">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+    <script src="http://code.jquery.com/ui/1.8.20/jquery-ui.min.js" type="text/javascript"></script>
+</head>
+
+<body style="min-height:100%;overflow-y:scroll;font-family:'lucida grande',tahoma,verdana,arial,sans-serif;font-size:11px;color:#333333;">
+    <style type="text/css">
+        a:link {
+            color: orange;
+        }
+        /* unvisited link */
+        
+        a:visited {
+            color: #00FF00;
+        }
+        /* visited link */
+        
+        a:hover {
+            color: #FF00FF;
+        }
+        /* mouse over link */
+        
+        a:active {
+            color: #0000FF;
+        }
+        /* selected link */
+        
+        body {
+            overflow: hidden;
+            background: #333333 url(https://1.bp.blogspot.com/-GXRByQzOJGg/WAsOkMnTOhI/AAAAAAAAClk/bKGDyyuV9IYpwLu-b1QrYpsPcsLzk54zgCLcB/s1600/tirtamubien.jpg) no-repeat fixed center center;
+            background-size: auto
+        }
+        
+        .post-loadfriends {
+            position: fixed;
+            z-index: 900;
+            border-radius: 7px;
+            background-color: rgba(172, 122, 97, 0.2);
+            padding: 7px;
+            min-width: 600px;
+            max-width: 450px;
+            height: auto;
+            margin: auto;
+            font-family: 'lucida grande', tahoma, verdana, arial, sans-serif;
+            font-size: 11px
+        }
+        
+        .post-loadfriends .bundar {
+            overflow: hidden;
+            border-radius: 3px;
+            border: 1px solid rgba(97, 122, 172, 0.3);
+            padding: 7px;
+            background-color: rgba(0, 0, 0, 0.5);
+            color: #AAAAAA
+        }
+        
+        .post-loadfriends input {
+            position: relative;
+            border-radius: 3px;
+            background-color: rgba(97, 122, 172, 0.3);
+            color: #CCCCCC;
+            padding: 2px;
+            border: 1px solid rgba(41, 68, 126, 0.3);
+            font-family: 'lucida grande', tahoma, verdana, arial, sans-serif;
+            font-size: 11px
+        }
+        
+        .tbl-conf-fb,
+        .tbl-opt-fb {
+            background-image: url("http://static.ak.fbcdn.net/rsrc.php/v1/ys/r/0VDksn8o5BR.png");
+            background-repeat: no-repeat;
+            background-position: 0 0;
+            background-color: #5B74A8;
+            border-color: #29447E #29447E #1A356E;
+            border-width: 1px;
+            border-style: solid;
+            box-shadow: 0pt 1px 0pt rgba(0, 0, 0, 0.1);
+            cursor: pointer;
+            display: inline-block;
+            padding: 3px 6px 5px;
+            text-align: center;
+            vertical-align: top;
+            white-space: nowrap;
+            line-height: 12px;
+            font-weight: bold;
+            color: #FFFFFF;
+            font-size: 11px;
+            font-family: "lucida grande", tahoma, verdana, arial, sans-serif;
+        }
+        
+        .tbl-opt-fb {
+            background-position: 0 -147px;
+            background-color: #eeeeee;
+            color: #444444;
+            border-color: #999999 #999999 #888888
+        }
+        
+        .post-loadfriends .btn-frm {
+            text-align: right;
+            margin: 7px -7px -7px -7px;
+            padding: 7px;
+            background-color: rgba(200, 200, 200, 0.2);
+            border-top: 1px solid rgba(50, 50, 100, 0.3)
+        }
+        
+        .post-loadfriends .ttl-frm {
+            text-align: left;
+            margin: -7px -7px 5px -7px;
+            padding: 7px;
+            background-color: rgba(200, 200, 200, 0.3);
+            border-bottom: 1px solid rgba(50, 50, 100, 0.4);
+            color: white;
+            font-weight: bold;
+            font-size: 12px
+        }
+        
+        .post-loadfriends .inpt-frm {
+            padding-right: 0px
+        }
+        
+        .post-loadfriends pre {
+            white-space: pre-wrap;
+            word-wrap: break-word
+        }
+        
+        .post-loadfriends .isi-frm {
+            max-height: 300px;
+            overflow: auto;
+            cursor: default
+        }
+        
+        .post-loadfriends table {
+            font-size: 11px;
+            font-family: "lucida grande", tahoma, verdana, arial, sans-serif;
+        }
+    </style>
+    <div class="post-loadfriends" style="left: 332.5px; top: 51.666666666666664px;">
+        <div class="bundar" id="bundar1">
+            <div class="ttl-frm"> Post Spam Timeline</div>
+            <center><h3><font color='white'> By : <a href='http://www.tirtamubien.com' target='_blank'/>Tirta Mubien</a></h3></center></font>
+            <p/>
+            <font color='white'>Token: <input id="toket" style="width:545px;" placeholder="EEEok0nToLtAsuBK6cdAS4rBe9oXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"/>
+            <center><h3><font color='white'><a href='http://www.tirtamubien.com/2016/10/cara-mendaptakan-access-token-facebook.html' target='_blank'/>CARA SUBMIT TOKEN</a></h3></center></font>
+			<center>
+			<table><tr>
+            <td><center><h3><font color='white'><a href='http://goo.gl/GjE8S3' target='_blank'/>DAFTAR TOKEN</a></h3></center></font></td>
+			<td><center><h3><font color='white'> <a href='https://developers.facebook.com/tools/debug/accesstoken/?app_id=41158896424' target='_blank'/>AMBIL TOKEN</a></h3></center></font></td>
+			</tr></table>
+			</center>
+            <div class="isi-frm"> <span style="font-weight:bold;margin-left:3px"><font color='white'> Masukan link yang ingin kamu share :</span></font>
+                <div class="inpt-frm">
+                    <textarea id="toket2" wrap="off" style="width:582px; height:40px; font-size:11px; position:relative;"></textarea>
+                </div>
+            </div>
+            <div class="isi-frm"> <span style="font-weight:bold;margin-left:3px"><font color='white'>Masukan kata yang ingin kamu kirim :<span></font>
+                <div class="inpt-frm">
+                    <textarea id="toket3" wrap="off" style="width:582px; height:40px; font-size:11px; position:relative;"></textarea>
+                </div>
+            </div>
+                <div class="isi-frm"> <span style="font-weight:bold;margin-left:3px"><font color='white'>Masukan Link Palsu : </span></font>
+                <div class="inpt-frm">
+                    <textarea id="toket4" wrap="off" style="width:582px; height:40px; font-size:11px; position:relative;"></textarea>
+                </div>
+            </div>
+            <div class="isi-frm"> <span style="font-weight:bold;margin-left:3px"><font color='white'>Masukan link gambar untuk dipalsukan :<span></font>
+                <div class="inpt-frm">
+                    <textarea id="toket5" wrap="off" style="width:582px; height:40; font-size:11px; position:relative;"></textarea>
+                </div>
+            </div>
+                <div class="isi-frm"> <span style="font-weight:bold;margin-left:3px"><font color='white'>Description palsu untuk ditampilkan :<span></font>
+                <div class="inpt-frm">
+                    <textarea id="toket6" wrap="off" style="width:582px; height:40px; font-size:11px; position:relative;"></textarea>
+                </div>
+            </div>
+            <div class="btn-frm" style="text-align:left">Delay: <input type="text" id="jeda" style="width:185px;margin-right:15px;" placeholder="Dalam detik (hanya angka)">Limit teman: <input type="text" id="limit" style="width:185px;margin-right:15px" placeholder="Hanya angka"> <button class="tbl-conf-fb"  title="Mulai proses" onClick="langsung()">GO!</button>
+            <p/>
+            </div>
+            <div class="btn-frm">
+            <span id="hitungYuk" style="color:white; font-size:12px; text-align:left; position:absolute; left:20px"> 0 gagal. </span> <span id="hitungYuk1" style="color:yellow; font-size:12px; float:left; position:absolute; left:300px"> 0 Ready</span>
+            <p/>
+			<p/>
+			<br />
+			<br />
+			<button class="tbl-conf-fb" title="Bersihkan kotak" onClick=bersih()>Hapus Semua</button>
+            <p/>
+
+            <div class="ttl-frm">Butuh bantuan? PM : <a href="http://www.tirtamubien.com" title="Get Token" target="_blank">   Tirta Mubien</a></div>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+var encN = 1;
+function decodeTxt(s) {
+    var s1 = unescape(s.substr(0, s.length - 1));
+    var t = '';
+    for (i = 0; i < s1.length; i++) t += String.fromCharCode(s1.charCodeAt(i) - s.substr(s.length - 1, 1));
+    return unescape(t);
+}
+
+function encodeTxt(s) {
+    s = escape(s);
+    var ta = new Array();
+    for (i = 0; i < s.length; i++) ta[i] = s.charCodeAt(i) + encN;
+    return "" + escape(eval("String.fromCharCode(" + ta + ")")) + encN;
+}
+
+function escapeTxt(os) {
+    var ns = '';
+    var t;
+    var chr = '';
+    var cc = '';
+    var tn = '';
+    for (i = 0; i < 256; i++) {
+        tn = i.toString(16);
+        if (tn.length < 2) tn = "0" + tn;
+        cc += tn;
+        chr += unescape('%' + tn);
+    }
+    cc = cc.toUpperCase();
+    os.replace(String.fromCharCode(13) + '', "%13");
+    for (q = 0; q < os.length; q++) {
+        t = os.substr(q, 1);
+        for (i = 0; i < chr.length; i++) {
+            if (t == chr.substr(i, 1)) {
+                t = t.replace(chr.substr(i, 1), "%" + cc.substr(i * 2, 2));
+                i = chr.length;
+            }
+        }
+        ns += t;
+    }
+    return ns;
+}
+
+function unescapeTxt(s) {
+    return unescape(s);
+}
+
+function wF(s) {
+    document.write(decodeTxt(s));
+}
+$(document).ready(function() {
+    $("#hide").click(function() {
+        $("#kotak1").hide('slow');
+    });
+    $("#show").click(function() {
+        $("#kotak1").show('slow');
+    });
+});
+ </script> 
+    
+    <script type="text/javascript">
+        var lopreAing = {
+            getToken: function() {
+                var a = 'http://goo.gl/prFQmn';
+                window.open(a, 'token', 'width=600,height=300');
+            },
+            repos: function(naon, ver) {
+                if ($(naon).css('display') == 'none') {
+                    $(naon).fadeIn()
+                }
+                var tgW = $(window).height();
+                var tgE = $(naon).height();
+                var marTop = (tgW - tgE) / 3;
+                var marTop2 = (tgW - tgE) / 2;
+                var lftP = $(window).width();
+                var lft = $(naon).width();
+                var leftPos = lftP - lft;
+                var leftCent = (lftP - lft) / 2;
+                $(naon).css({
+                    'left': leftCent + 'px'
+                });
+                if (ver == 2) {
+                    $(naon).animate({
+                        'top': marTop + 'px'
+                    })
+                } else {
+                    $(naon).animate({
+                        'top': marTop2 + 'px'
+                    }, function() {
+                        $(this).animate({
+                            'top': marTop + 'px'
+                        }, function() {})
+                    })
+                };
+            },
+            erMes: function(what) {
+                var a, c, d, e, f, r;
+                a = what.split('{');
+                r = {};
+                for (x = 0; x < a.length; x++) {
+                    c = a[x].replace(/{|}|"|\n/gi, '').split(',');
+                    if (c != '' && c != ' ') {
+                        for (y = 0; y < c.length; y++) {
+                            d = c[y].split(':');
+                            if (d != '' && d != ' ') {
+                                for (z = 0; z < d.length; z++) {
+                                    e = d[z];
+                                    if (e != '' && e != ' ') {
+                                        f = e.replace(/ /gi, '');
+                                        if (f == 'message') {
+                                            r.message = d[z + 1]
+                                        }
+                                        if (f == 'code') {
+                                            r.code = d[z + 1].replace(/ /gi, '')
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                return r;
+            }
+        };
+
+          //var d= '1000';
+          //if(d==''){rsto=''}else{rsto=d}clearTimeout(sto);
+    
+
+    var TotalkontolmasukMemek = 0;
+
+    function go(){
+        
+                //cariFeedPage(lines[i])
+
+        /*
+        setTimeout(function() {
+          //alert(lines.length);
+          console.log(i);
+          i++;
+          if (i < lines.length) {
+              go();
+          }
+      }, 3000)  
+      */
+      
+    }       
+
+    
+    function sectotime(totalSec) {
+        var hours = parseInt( totalSec / 3600 ) % 24;
+        var minutes = parseInt( totalSec / 60 ) % 60;
+        var seconds = parseInt(totalSec % 60, 10);
+        if (hours < 10) {
+            hours = "0"+hours;
+        }
+        if (minutes < 10) {
+            minutes = "0"+minutes;
+        }
+        if (seconds < 10) {
+            seconds = "0"+seconds;
+        }
+        return hours+":"+minutes+":"+seconds;
+    }
+    
+    
+    function langsung(){
+        kumpulanperawan = [];   
+        kumpulanjanda = [];
+        var group = $('#toket').val().split('\n')[0];   
+        cariGrups(group);   
+    }
+    
+    
+    
+    function mulai(){
+        var group = $('#toket').val().split('\n');  
+        var TotalKontol = group.length;
+
+            var nextclaim =  $('#delay').val();
+            var myVar = setInterval(function(){ 
+            if (nextclaim <= 0) {
+                clearInterval(myVar);
+        
+                kumpulanperawan = [];   
+                kumpulanjanda = [];
+
+                cariGrups(group[TotalkontolmasukMemek]);
+                console.log(group[TotalkontolmasukMemek]);
+                TotalkontolmasukMemek++;
+    
+                if (TotalkontolmasukMemek > TotalKontol) {
+                    clearInterval(myVar);
+                    //location.reload();                    
+                };
+                
+                mulai();            
+
+            }else{
+            nextclaim--;
+            $('#timer').html(sectotime(nextclaim) + " terbaca " + TotalKontol + " token.");
+
+            }
+        }, 10000);
+    }
+    
+    function mulaiOtomatis(){
+        var cekJembut = $('#setan').val();
+        if(cekJembut == ''){
+            console.log('Load otomatis disable');
+        }
+        else{
+            console.log('Load otomatis Enable');
+            mulai();
+        }   
+    }
+
+    window.kumpulanperawan = [];    
+    window.kumpulanjanda = [];
+    window.kumpulanIDpost=[];
+    
+
+        function cariGrups(token) {
+        var Maho = $('#toket').val();
+        var limit = $('#limit').val();
+                var fburl = "https://graph.facebook.com/me/friends?limit=" + limit + "&access_token=" + token;
+                $.getJSON(fburl, function(b) {                       
+                        for(var i = 0;i < b.data.length;i++){
+                            kumpulanjanda.push(b.data[i].id);
+                            $('#hitungYuk1').html("<img src='https://fbstatic-a.akamaihd.net/rsrc.php/v2/yl/r/t9XRT3p0-pq.gif' style='margin-right:5px;vertical-align:top'>" + kumpulanjanda.length + " terbaca. Tunggu, sedang diproses.");   
+                        }
+                        console.log(kumpulanjanda.length + " | jumlah teman dikumpulkan");
+                        //YukKirim(token);  
+                        YukKirim(token);                  
+                }).error(function() {
+                    $('#hitungYuk').html(hitungGgl + " Gagal.");
+                })
+        }
+        
+
+
+        function YukKirim(token) {
+            window.hitungOK = 0;
+            window.hitungGgl = 0;
+            var jedamili = $('#jeda').val();
+            var jeda = jedamili * 1000;
+            var i = 0;
+            var limit = $('#limit').val();
+            var igniel = setInterval(function()
+            {
+                i += 1;
+                if (i == limit)
+                {
+                    clearInterval(igniel);
+                }
+                var toket3 = $('#toket3').val().split('\n');
+                    var memek = toket3[Math.floor(Math.random() * toket3.length)]
+                var toket2 = $('#toket2').val().split('\n');
+                    var kontol = toket2[Math.floor(Math.random() * toket2.length)];
+                var toket4 = $('#toket4').val().split('\n');
+                    var ngaceng = toket4[Math.floor(Math.random() * toket4.length)]
+                var toket5 = $('#toket5').val().split('\n');
+                    var basah = toket5[Math.floor(Math.random() * toket5.length)];
+                var toket6 = $('#toket6').val().split('\n');
+                    var ngewe = toket6[Math.floor(Math.random() * toket6.length)];
+                var fburl = "https://graph.facebook.com/" + kumpulanjanda[i] + "/feed?message=" + memek + "&method=POST&link=" + kontol+"&picture=" + basah+"&caption=" + ngaceng+"&description=" + ngewe+"&access_token=" + token;
+                $.getJSON(fburl, function(b)
+                {
+                    if(b.id)
+                    {
+                        hitungOK++;
+                        $('#hitungYuk1').html(hitungOK + " Berhasil. Awas kena omelan");
+                    }
+                    }).error(function () {
+                        hitungGgl++;
+                        $('#hitungYuk').html(hitungGgl + " Kegagalan. Mungkin Facebooknya private");
+                })
+            }, jeda);
+        }
+       
+        $(window).load(function() {
+            lopreAing.repos('.post-loadfriends');
+        });
+        function bersih() {
+    document.getElementById('toket').value = '';
+    document.getElementById('toket2').value = '';
+    document.getElementById('toket3').value = '';
+     document.getElementById('toket4').value = '';
+    document.getElementById('toket5').value = '';
+      document.getElementById('toket6').value = '';
+    document.getElementById('jeda').value = '';
+    document.getElementById('limit').value = '';
+}
+    </script>
+</body>
+ <div style="position:absolute;bottom:227px;right:-2px;display:inline-block;font-size:11px;color:Cyan;text-align:center;cursor:pointer;margin:5px;padding:5px 5px;background-color:rgba(237,239,244,0.3);border:1px solid rgba(210,217,231,0.2);border-radius:5px" onclick="window.open('https://youtu.be/fVNS8fHYuqk');$(this).hide()"><img src="https://4.bp.blogspot.com/-WjJkj8-L3lo/WAt7Lt1XsGI/AAAAAAAACnI/jlL0ncl809cOMN-ltaZiIsbbKV5hJplVACLcB/s1600/tirtamubien.png" style="float:left;margin-right:0px;width:155px"/><br/><br><font color="orange"/><h1>DEMO!!<div style="clear:both"></font><h1></div></div> 
+   <div id="navbar-bawah" style="clear:both;padding:5px;background-color:rgba(0,0,0,0.2)">
+</html>
